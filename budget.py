9903b055f5fdf62d68d506d0398e69b3e5370d7d
jjoniel <jonieljerome@hotmail.com>
2025-03-03 21:51:01 -0500

    monkeychanges

--- budget.py
+++ budget.py
@@ -130,5 +130,5 @@ def add_expense(date, entity, money):
 
 if __name__ == "__main__":
     extract_all_info()
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    app.run(host='0.0.0.0', port=8080, debug=False)
 
