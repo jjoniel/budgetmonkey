01581e33121b8025ab78ced63ee93f24b7b0ac54
Sanjay <san68bot@gmail.com>
2025-04-21 11:00:21 -0400

    Revert "stats and shi"
    This reverts commit ff810e85bb671569a4f66794270eae6bae79b579.

--- transaction.py
+++ transaction.py
@@ -10,7 +10,7 @@ class Transaction:
     - date (datetime.date) : the data of the transaction
 
     """
-    def __init__(self, net, vendor, date, category="Uncategorized"):
+    def __init__(self, net, vendor, date):
         """
         The constructor creates a transaction object with a net value,
         an associated vendor, and a justifying description
@@ -24,7 +24,6 @@ class Transaction:
         self.net = net
         self.vendor = vendor
         self.date = date
-        self.category = category
 
     def __str__(self):
         return (
@@ -37,7 +36,9 @@ class Transaction:
         )
 
     def csv(self):
-        return f"{self.net},{self.vendor},{self.date.year},{self.date.month},{self.date.day},{self.category}"
+        return (
+            f"{self.net},{self.vendor},{self.date.year},{self.date.month},{self.date.day}"
+        )
 
     def __lt__(self, other):
         if self.date.year < other.date.year:
